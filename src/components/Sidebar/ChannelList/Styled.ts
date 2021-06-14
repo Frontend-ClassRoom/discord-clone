f3747ca6336f08d47646baa0a6daf7263491d217
전병길 <jeonbg@kakao.com>
2021-06-15 00:02:55 +0900

    fix (styles) : ChannelList 수정, 코드 정리 1. Channel List Hover Event 로직수정 2. Channel 이름 변경시 Dispatch 추가 3. 불필요한 코드 수정

--- src/components/Sidebar/ChannelList/Styled.ts
+++ src/components/Sidebar/ChannelList/Styled.ts
@@ -6,6 +6,10 @@ export const ChannelListItem = {
     transition: background 0.3s ease;
     &:hover {
       background: #363a3f;
+      > .channel-icons {
+        visibility: visible;
+        opacity: 1;
+      }
     }
   `,
   Title: styled.h4`
@@ -22,15 +26,18 @@ export const ChannelListItem = {
     padding: 8px;
   `,
 
-  Icons: styled.div<{ hovered: boolean }>`
+  Icons: styled.div`
+    // className = channel-icons
+    // Panel에 Hover시 visibility, opaticy 변경
+    visibility: hidden;
     position: absolute;
     right: 0;
     top: 50%;
     padding-right: 20px;
     color: gray;
-    opacity: ${({ hovered }) => (hovered ? 1 : 0)};
     transform: translateY(-50%);
-    transition: all 0.3s ease;
+    opacity: 0;
+    transition: opacity 0.3s ease;
     > * {
       margin-left: 5px;
       cursor: pointer;
